cc21ca395f9f52112afe85108dbbfaa8aeb79903
Sig <drakonsmert@gmail.com>
2018-11-13 00:28:25 +0300

    functional macros
     - lambda in macros
     - function set and replace macros

--- pyinclude/pyinclude.py
+++ pyinclude/pyinclude.py
@@ -40,15 +40,7 @@ class SafeExecuteRecurseLocals:
             loc = SafeExecuteRecurseLocals(**self.__dict__)
             del loc.__dict__[key]
             try:
-                env = {}
-                env[r'locals']   = None
-                env[r'globals']  = None
-                env[r'__name__'] = None
-                env[r'__file__'] = None
-                env[r'__builtins__'] = None
-                
-                try_py = re.sub(IncludeParser.safe_rx, r'', value)
-                value = eval(try_py, env, loc)
+                value = IncludeParser.safe_eval(value, loc)
             except:
                 if isinstance(value, str) or isinstance(value, six.string_types):
                     value = IncludeParser.html_parser.unescape(value)
@@ -73,9 +65,9 @@ class IncludeParser:
         r'else': re.compile(r'^\s*else\s*$', re.M or re.S),
         r'endif': re.compile(r'^\s*endif|fi\s*$', re.M or re.S),
         r'undef': re.compile(r'^\s*(?:del|undef|remove)\s+([A-z][A-z_0-9]*)\s*$', re.M or re.S),
-        r'define': re.compile(r'^\s*(?:def|define|declare)\s+([A-z][A-z_0-9]*)(?:(?:\s*=\s*|\s+)([^$]+))?\s*$', re.M or re.S) # must be last - is the broadest definition
+        r'define': re.compile(r'^\s*(?:def|define|declare|macro|macros)\s+([A-z][A-z_0-9]*)(?:(?:(\([^\)]+\))?\s*[\=\:]?\s*|\s*[\=\:]\s*|\s+)([^$]+))?\s*$', re.M or re.S) # must be last - is the broadest definition
     }
-    safe_rx = re.compile(r'eval|lambda|__[A-z]+__', re.M or re.S)
+    safe_rx = re.compile(r'eval|__[A-z]+__', re.M or re.S)
     html_parser = HTMLParser()
 
     def clear():
@@ -87,28 +79,95 @@ class IncludeParser:
         IncludeParser.state_table = []
     clear = staticmethod(clear)
 
+    def argsParse(s):
+        local = { r'count' : 0, '\'' : False, r'"' : False, '\'\'\'' : False, r'"""' : False}
+        def st(val, state):
+            if val == '\'' and not (local[r'"'] or local['\'\'\''] or local[r'"""']):
+                local[val] = not local[val]
+            if val == r'"' and not (local['\''] or local['\'\'\''] or local[r'"""']):
+                local[val] = not local[val]
+            if val == '\'\'\'' and not (local[r'"'] or local['\''] or local[r'"""']):
+                local[val] = not local[val]
+            if val == r'"""' and not (local[r'"'] or local['\''] or local['\'\'\'']):
+                local[val] = not local[val]
+
+            if val == r'(':
+                local[r'count'] += 1
+                if state == r'none':
+                    return r'in_args'
+            elif val == r')':
+                local[r'count'] -= 1
+                if state == r'in_args' and local[r'count'] == 0:
+                    return r'none'
+            return state
+        return lazy.lazy(s).group(st, lambda a, v, s: a + v, r'').reduce(lambda x, y : x + [y], [])
+    argsParse = staticmethod(argsParse)
+
+    def toString(obj):
+        if isinstance(obj, str) or isinstance(obj, six.string_types):
+            return obj
+        return str(obj)
+    toString = staticmethod(toString)
+
+    def safe_eval(cmd, loc = None):
+        if loc == None:
+            loc = SafeExecuteRecurseLocals(**IncludeParser.macros)
+        env = {}
+        env[r'locals']   = None
+        env[r'globals']  = None
+        env[r'__name__'] = None
+        env[r'__file__'] = None
+        env[r'__builtins__'] = None
+
+        cmd = re.sub(IncludeParser.safe_rx, r'', cmd)
+        return eval(cmd, env, loc)
+    safe_eval = staticmethod(safe_eval)
+
+    def safe_bool_eval(cmd, loc = None):
+        if loc == None:
+            loc = SafeExecuteRecurseLocals(**IncludeParser.macros)
+        try:
+            return bool(IncludeParser.safe_eval(cmd, loc))
+        except:
+            print(r'Wrong if expressin')
+        return False
+    safe_bool_eval = staticmethod(safe_bool_eval)
+
     def replaceMaros(data, loc=None):
-        if not isinstance(data, str) or not isinstance(data, six.string_types):
-            data = str(data)
+        data = IncludeParser.toString(data)
         if len(data) > 0:
             if loc == None:
                 loc = SafeExecuteRecurseLocals(**IncludeParser.macros)
-            rx = re.compile(r'(?:^|(?<=\W))(' + r'|'.join(IncludeParser.macros.keys()) + r')(?=\W|$)', flags=re.M or re.S)
+            rx = re.compile(r'(?:^|(?<=\W))(' + r'|'.join(loc.__dict__.keys()) + r')(?=\W|$)(\([^\)]+\))?', flags=re.M or re.S)
             start = 0
             end = 0
             out = r''
             for m in re.finditer(rx, data):
                 end = m.start()
                 if start < end:
-                    out += data[start:end]
-                macros_name = m.group(1)
+                    out += m.string[start:end]
+                macros_name, args = m.groups()
                 if macros_name != None: 
                     value = loc[macros_name]
                     if value == None:
-                        out += macros_name
+                        out += m.string[m.start():m.end()]
                     else:
-                        if not isinstance(value, str) or not isinstance(value, six.string_types):
-                            value = str(value)
+                        if not isinstance(value, str) and not isinstance(value, six.string_types):
+                            if callable(value):
+                                if args == None:
+                                    value = IncludeParser.macros[macros_name]
+                                else:
+                                    part = m.string[m.start():]
+                                    parsed = IncludeParser.argsParse(part)
+                                    try:
+                                        value = IncludeParser.toString(IncludeParser.safe_eval(macros_name + parsed[1], loc))
+                                        out += value
+                                        start = m.start() + len(parsed[0]) + len(parsed[1])
+                                        continue
+                                    except:
+                                        value = m.string[m.start():m.end()]
+                            else:
+                                value = str(value)
                         out += value
 
                 start = m.end()
@@ -236,7 +295,7 @@ class IncludeParser:
                     count = 1
                 if full_path == None:
                     return False
-                path = lazy.lazy(full_path).group(lazy.lazy_stateTable(IncludeParser.strings_state), lambda a, v, s : a + v, '').reduce(lambda x, y : IncludeParser.concat_strings(x, y), '')
+                path = lazy.lazy(full_path).group(lazy.lazy_stateTable(IncludeParser.strings_state), lambda a, v, s : a + v, r'').reduce(lambda x, y : IncludeParser.concat_strings(x, y), r'')
                 
                 if not os.path.isabs(path):
                     path = os.path.join(self.root, path)
@@ -247,11 +306,14 @@ class IncludeParser:
                     out += self.includeFile(name, count)
                 return out
             elif cmd == r'define':
-                macros, value = ret.groups()
+                macros, args, value = ret.groups()
                 if macros == None:
                     return False
                 if value == None:
                     value = 1
+                if args != None:
+                    groups = IncludeParser.argsParse(code)
+                    value = r'(lambda ' + groups[1][1:-1] + r' : ' + r''.join(groups[2:]) + r')'
                 IncludeParser.macros[macros] = value
                 return True
             elif cmd == r'undef':
@@ -269,7 +331,7 @@ class IncludeParser:
                     expression = ret.groups()
                     if expression != None:
                         expression = expression[0].strip()
-                        value = self.safe_bool_eval(expression)
+                        value = IncludeParser.safe_bool_eval(expression)
                 self.if_stack.append(value)
                 if  value == False:
                     return r'skip'
@@ -283,7 +345,7 @@ class IncludeParser:
                     expression = ret.groups()
                     if expression != None:
                         expression = expression[0].strip()
-                        value = self.safe_bool_eval(expression)
+                        value = IncludeParser.safe_bool_eval(expression)
                 self.if_stack[-1] = value
                 if  value == False:
                     return r'skip'
@@ -306,21 +368,6 @@ class IncludeParser:
                     return r'unskip' if parent else r'skip'
                 return True
             break
-        return False
-
-    def safe_bool_eval(self, cmd):
-        try:
-            env = {}
-            env[r'locals']   = None
-            env[r'globals']  = None
-            env[r'__name__'] = None
-            env[r'__file__'] = None
-            env[r'__builtins__'] = None
-            
-            cmd = re.sub(IncludeParser.safe_rx, r'', cmd)
-            return bool(eval(cmd, env, SafeExecuteRecurseLocals(**IncludeParser.macros)))
-        except:
-            print(r'Wrong if expressin')
         return False
 
     def includeFile(self, path, count):
